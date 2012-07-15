2bf1a90a1406ecbd1bf2e4706c7216fd1414df9e
Matthias Braun <matze@braunis.de>
2012-07-15 10:57:29 +0200

    test non-ascii chars in attributes

--- src/org/wonderly/doclets/test/FooBar.java
+++ src/org/wonderly/doclets/test/FooBar.java
@@ -1,7 +1,10 @@
 package org.wonderly.doclets.test;
 
 /**
- * This interface has no use whatsoever
+ * This interface has no use whatsoever.
+ * <a name="cööl"><b>böld extended chärs wühü</b></a>.
+ *
+ * Look <a href="http://en.wikipedia.org/wiki/Motörhead">here</a>.
  */
 public interface FooBar {
 	/** does nothing */
